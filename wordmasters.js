400c5003918013db6c72b519e7c5df86b51970c3
muhammadsalah7 <muhammadsalah217@gmail.com>
2024-02-07 05:10:21 +0200

    user input is now finished with enter and backspace implemented

---
+++ wordmasters.js
@@ -0,0 +1,59 @@
+const ANSWER_LENGTH = 5;
+const ROUNDS = 6;
+const letters = document.querySelectorAll(".scoreboard-letter");
+const loadingDiv = document.querySelector(".info-bar");
+
+async function init() {
+  let currentGuess = "";
+  let currentRow = 0;
+
+  function addLetter(letter) {
+    if (currentGuess.length < ANSWER_LENGTH) {
+      //add letter to the end
+      currentGuess += letter;
+    } else {
+      //replace  the last letter
+      currentGuess = currentGuess.slice(0, -1) + letter;
+    }
+    letters[ANSWER_LENGTH * currentRow + currentGuess.length - 1].innerText =
+      letter;
+  }
+
+  async function commit() {
+    if (currentGuess.length !== ANSWER_LENGTH) {
+      //do nothing
+      return;
+    }
+    // TODO validate the word
+
+    // TODO do all the marking as "correct" , "close" or "wrong"
+
+    // TODO did they win or lose
+
+    currentRow++;
+    currentGuess = "";
+  }
+
+  function backspace() {
+    currentGuess = currentGuess.slice(0, -1);
+    letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = "";
+  }
+
+  document.addEventListener("keydown", function handleKeyPress(event) {
+    const action = event.key;
+    if (action === "Enter") {
+      commit();
+    } else if (action === "Backspace") {
+      backspace();
+    } else if (isLetter(action)) {
+      addLetter(action.toUpperCase());
+    } else {
+      //do nothing
+    }
+    return undefined;
+  });
+}
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
+}
+init();
