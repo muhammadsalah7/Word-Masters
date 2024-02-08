75664f848a374bcd311fdfd6322356ba9d586a0b
muhammadsalah7 <muhammadsalah217@gmail.com>
2024-02-08 05:18:11 +0200

    implemented the correct, close and wrong functionalities

--- wordmasters.js
+++ wordmasters.js
@@ -9,7 +9,7 @@ async function init() {
   const res = await fetch("https://words.dev-apis.com/word-of-the-day");
   const resObj = await res.json();
   const word = resObj.word.toUpperCase();
-  console.log(word);
+  const wordParts = word.split("");
   setLoading(false);
 
   function addLetter(letter) {
@@ -31,8 +31,28 @@ async function init() {
     }
     // TODO validate the word
 
-    // TODO do all the marking as "correct" , "close" or "wrong"
+    const guessParts = currentGuess.split("");
+    const map = makeMap(wordParts);
 
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      //mark as correct
+      if (guessParts[i] === wordParts[i]) {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("correct");
+        map[guessParts[i]]--;
+      }
+    }
+
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      //mark as correct
+      if (guessParts[i] === wordParts[i]) {
+        // do nothing
+      } else if (wordParts.includes(guessParts[i]) && map[guessParts[i]] > 0) {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("close");
+        map[guessParts[i]]--;
+      } else {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("wrong");
+      }
+    }
     // TODO did they win or lose
 
     currentRow++;
@@ -64,5 +84,18 @@ function isLetter(letter) {
 
 function setLoading(isLoading) {
   loadingDiv.classList.toggle("show", isLoading);
+}
+
+function makeMap(array) {
+  const obj = {};
+  for (let i = 0; i < array.length; i++) {
+    const letter = array[i];
+    if (obj[letter]) {
+      obj[letter]++;
+    } else {
+      obj[letter] = 1;
+    }
+  }
+  return obj;
 }
 init();
