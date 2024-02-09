c744527e86cfedb7343a99445c0463ab15556966
muhammadsalah7 <muhammadsalah217@gmail.com>
2024-02-09 08:44:58 +0200

    validating the word and alerting the user are done

--- wordmasters.js
+++ wordmasters.js
@@ -2,15 +2,19 @@ const ANSWER_LENGTH = 5;
 const ROUNDS = 6;
 const letters = document.querySelectorAll(".scoreboard-letter");
 const loadingDiv = document.querySelector(".info-bar");
+const brand = document.querySelector(".brand");
 
 async function init() {
   let currentGuess = "";
   let currentRow = 0;
+  let isLoading = true;
   const res = await fetch("https://words.dev-apis.com/word-of-the-day");
   const resObj = await res.json();
   const word = resObj.word.toUpperCase();
   const wordParts = word.split("");
+  let done = false;
   setLoading(false);
+  isLoading = false;
 
   function addLetter(letter) {
     if (currentGuess.length < ANSWER_LENGTH) {
@@ -29,7 +33,22 @@ async function init() {
       //do nothing
       return;
     }
-    // TODO validate the word
+
+    //validate the word
+    isLoading = true;
+    setLoading(true);
+    const res = await fetch("https://words.dev-apis.com/validate-word", {
+      method: "POST",
+      body: JSON.stringify({ word: currentGuess }),
+    });
+    const resObj = await res.json();
+    const validWord = resObj.validWord;
+    isLoading = false;
+    setLoading(false);
+    if (!validWord) {
+      markInvalidWord();
+      return;
+    }
 
     const guessParts = currentGuess.split("");
     const map = makeMap(wordParts);
@@ -53,9 +72,17 @@ async function init() {
         letters[currentRow * ANSWER_LENGTH + i].classList.add("wrong");
       }
     }
-    // TODO did they win or lose
 
     currentRow++;
+    if (currentGuess === word) {
+      alert("you win");
+      brand.classList.add("winner");
+      done = true;
+      return;
+    } else if (currentRow === ROUNDS) {
+      alert(`You Lose, word of the day was ${word}`);
+      done = true;
+    }
     currentGuess = "";
   }
 
@@ -64,7 +91,20 @@ async function init() {
     letters[ANSWER_LENGTH * currentRow + currentGuess.length].innerText = "";
   }
 
+  function markInvalidWord() {
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      letters[currentRow * ANSWER_LENGTH + i].classList.remove("invalid");
+      setTimeout(function () {
+        letters[currentRow * ANSWER_LENGTH + i].classList.add("invalid");
+      }, 10);
+    }
+  }
+
   document.addEventListener("keydown", function handleKeyPress(event) {
+    if (done || isLoading) {
+      //do nothing
+      return;
+    }
     const action = event.key;
     if (action === "Enter") {
       commit();
