5e5d2635bf4d695a777e394d87385a058db9b387
Muhammad Salah <muhammadsalah217@gmail.com>
2024-02-11 03:55:55 +0200

    typo fix

--- wordmasters.js
+++ wordmasters.js
@@ -8,7 +8,7 @@ async function init() {
   let currentGuess = "";
   let currentRow = 0;
   let isLoading = true;
-  const res = await fetch("https://words.dev-apis.com/word-of-the-day?");
+  const res = await fetch("https://words.dev-apis.com/word-of-the-day");
   const resObj = await res.json();
   const word = resObj.word.toUpperCase();
   const wordParts = word.split("");
